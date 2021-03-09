657bf27ff8b1cae06b4da4e5075f3211473d2186
J5 <john.heyer@hightail.com>
2021-03-09 01:14:54 +0000

    updates

---
+++ terraform/gcp/ha_vpn/vpn_psks.tf
@@ -0,0 +1,8 @@
+resource "random_password" "PEER0_PSK" {
+  length           = 20
+  special          = false
+}
+resource "random_password" "PEER1_PSK" {
+  length           = 20
+  special          = false
+}
--- terraform/gcp/ha_vpn/vpn_tunnels.tf
+++ terraform/gcp/ha_vpn/vpn_tunnels.tf
@@ -4,7 +4,8 @@ resource "google_compute_vpn_tunnel" "VPN_TUN0" {
   vpn_gateway                     = var.vpn_gateway_name
   peer_external_gateway           = var.peer_gateway_name
   depends_on                      = [ google_compute_external_vpn_gateway.EXTERNAL_VPN_GATEWAY ]
-  shared_secret                   = var.peer0_psk
+  #shared_secret                   = var.peer0_psk
+  shared_secret                   = random_password.PEER0_PSK.result
   router                          = var.cloud_router_name
   peer_external_gateway_interface = 0
   vpn_gateway_interface           = 0
@@ -16,7 +17,8 @@ resource "google_compute_vpn_tunnel" "VPN_TUN1" {
   vpn_gateway                     = var.vpn_gateway_name
   peer_external_gateway           = var.peer_gateway_name
   depends_on                      = [ google_compute_external_vpn_gateway.EXTERNAL_VPN_GATEWAY ]
-  shared_secret                   = var.peer1_psk
+  #shared_secret                   = var.peer1_psk
+  shared_secret                   = random_password.PEER1_PSK.result
   router                          = var.cloud_router_name
   peer_external_gateway_interface = 1
   vpn_gateway_interface           = 1
