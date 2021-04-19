0145a9867d8a0ab73e7d061dd9b5448b7ec5568f
J5 <john.heyer@hightail.com>
2021-04-19 14:36:17 -0700

    random ip test

--- terraform/gcp/ha_vpn/router_interfaces.tf
+++ terraform/gcp/ha_vpn/router_interfaces.tf
@@ -1,23 +1,35 @@
-resource "random_integer" "INTERFACE_BASE" {
+resource "random_integer" "THIRD_OCTET_0" {
+  min = 2
+  max = 255
+}
+resource "random_integer" "THIRD_OCTET_1" {
+  min = 2
+  max = 255
+}
+resource "random_integer" "FOURTH_OCTET_BASE_0" {
   min = 0
-  max = 62
+  max = 63
+}
+resource "random_integer" "FOURTH_OCTET_BASE_1" {
+  min = 0
+  max = 63
 }
 
 # Create Cloud Router Point-to-Point Interfaces to use for Routing 
 resource "google_compute_router_interface" "ROUTER_INTERFACE0" {
-  name                            = "${var.region}-${var.peer_gateway_name}-int0"
-  region                          = var.region
-  #ip_range                        = var.peer0_local_ip
-  ip_range                        = random_integer.INTERFACE_BASE.result * 4 + 1
-  router                          = var.cloud_router_name
-  vpn_tunnel                      = google_compute_vpn_tunnel.VPN_TUN0.id
+  name          = "${var.region}-${var.peer_gateway_name}-int0"
+  region        = var.region
+  #ip_range     = var.peer0_local_ip
+  ip_range      = "169.254.${random_integer.THIRD_OCTET_0.result}.${random_integer.FOURTH_OCTET_BASE_0.result * 4 + 1}
+  router        = var.cloud_router_name
+  vpn_tunnel    = google_compute_vpn_tunnel.VPN_TUN0.id
 }
 resource "google_compute_router_interface" "ROUTER_INTERFACE1" {
-  name                            = "${var.region}-${var.peer_gateway_name}-int1"
-  region                          = var.region
+  name          = "${var.region}-${var.peer_gateway_name}-int1"
+  region        = var.region
   #ip_range                        = var.peer1_local_ip
-  ip_range                        = random_integer.INTERFACE_BASE.result * 4 + 5
-  router                          = var.cloud_router_name
-  vpn_tunnel                      = google_compute_vpn_tunnel.VPN_TUN1.id
+  ip_range      = "169.254.${random_integer.THIRD_OCTET_1.result}.${random_integer.FOURTH_OCTET_BASE_1.result * 4 + 1}
+  router        = var.cloud_router_name
+  vpn_tunnel    = google_compute_vpn_tunnel.VPN_TUN1.id
 }
 
