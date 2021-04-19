27e7bcd9fea246ea2acd57cf9aa3b9fe0d0fd529
J5 <john.heyer@hightail.com>
2021-04-19 15:07:23 -0700

    random ip test

--- terraform/gcp/ha_vpn/randomized_interfaces.tf
+++ terraform/gcp/ha_vpn/randomized_interfaces.tf
@@ -19,7 +19,7 @@ locals {
   peer0_third_octet  = random_integer.PEER0_THIRD_OCTET.result
   peer0_gcp_ip       = random_integer.PEER0_FOURTH_OCTET_BASE.result * 4 + 1
   peer0_bgp_ip       = random_integer.PEER0_FOURTH_OCTET_BASE.result * 4 + 2
-  peer1_third_octet  = random_integer.PEER1_THIRD.result
+  peer1_third_octet  = random_integer.PEER1_THIRD_OCTET.result
   peer1_gcp_ip       = random_integer.PEER1_FOURTH_OCTET_BASE.result * 4 + 1
   peer1_bgp_ip       = random_integer.PEER1_FOURTH_OCTET_BASE.result * 4 + 2
 }
