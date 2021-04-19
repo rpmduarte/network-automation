6e0b1ab8c36899eff6d20f05a5adb28db1c4dadd
J5 <john.heyer@hightail.com>
2021-04-19 15:09:51 -0700

    random ip test

--- terraform/gcp/ha_vpn/router_bgp_peers.tf
+++ terraform/gcp/ha_vpn/router_bgp_peers.tf
@@ -3,7 +3,7 @@ resource "google_compute_router_peer" "ROUTER_VPN_BGP0" {
   name                            = "${var.region}-${var.peer_gateway_name}-bgp0"
   router                          = var.cloud_router_name
   #peer_ip_address                 = var.peer0_remote_ip
-  peer_ip_address                 = local.peer0_bgp_ip
+  peer_ip_address                 = "169.254.${local.peer0_third_octet}.${local.peer0_bgp_ip}"
   peer_asn                        = var.peer0_bgp_asn
   advertised_route_priority       = var.peer0_adv_metric
   depends_on                      = [ google_compute_router_interface.ROUTER_INTERFACE0 ]
@@ -19,7 +19,7 @@ resource "google_compute_router_peer" "ROUTER_VPN_BGP1" {
   name                            = "${var.region}-${var.peer_gateway_name}-bgp1"
   router                          = var.cloud_router_name
   #peer_ip_address                 = var.peer1_remote_ip
-  peer_ip_address                 = local.peer1_bgp_ip
+  peer_ip_address                 = "169.254.${local.peer1_third_octet}.${local.peer1_bgp_ip}"
   peer_asn                        = var.peer1_bgp_asn
   advertised_route_priority       = var.peer1_adv_metric
   depends_on                      = [ google_compute_router_interface.ROUTER_INTERFACE1 ]
--- terraform/gcp/ha_vpn/router_interfaces.tf
+++ terraform/gcp/ha_vpn/router_interfaces.tf
@@ -4,7 +4,7 @@ resource "google_compute_router_interface" "ROUTER_INTERFACE0" {
   name          = "${var.region}-${var.peer_gateway_name}-int0"
   region        = var.region
   #ip_range     = var.peer0_local_ip
-  ip_range      = "169.254.${local.peer0_gcp_ip}/30"
+  ip_range      = "169.254.${local.peer0_third_octet}.${local.peer0_gcp_ip}/30"
   router        = var.cloud_router_name
   vpn_tunnel    = google_compute_vpn_tunnel.VPN_TUN0.id
 }
@@ -12,7 +12,7 @@ resource "google_compute_router_interface" "ROUTER_INTERFACE1" {
   name          = "${var.region}-${var.peer_gateway_name}-int1"
   region        = var.region
   #ip_range                        = var.peer1_local_ip
-  ip_range      = "169.254.${local.peer1_gcp_ip}/30"
+  ip_range      = "169.254.${local.peer1_third_octet}.${local.peer1_gcp_ip}/30"
   router        = var.cloud_router_name
   vpn_tunnel    = google_compute_vpn_tunnel.VPN_TUN1.id
 }
