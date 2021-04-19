056ee38b27b4144c64b3187a37c9386300ddb94f
J5 <john.heyer@hightail.com>
2021-04-19 14:52:31 -0700

    random ip test

--- terraform/gcp/ha_vpn/router_interfaces.tf
+++ terraform/gcp/ha_vpn/router_interfaces.tf
@@ -1,19 +1,3 @@
-resource "random_integer" "THIRD_OCTET_0" {
-  min = 2
-  max = 255
-}
-resource "random_integer" "THIRD_OCTET_1" {
-  min = 2
-  max = 255
-}
-resource "random_integer" "FOURTH_OCTET_BASE_0" {
-  min = 0
-  max = 63
-}
-resource "random_integer" "FOURTH_OCTET_BASE_1" {
-  min = 0
-  max = 63
-}
 
 # Create Cloud Router Point-to-Point Interfaces to use for Routing 
 resource "google_compute_router_interface" "ROUTER_INTERFACE0" {
@@ -33,9 +17,3 @@ resource "google_compute_router_interface" "ROUTER_INTERFACE1" {
   vpn_tunnel    = google_compute_vpn_tunnel.VPN_TUN1.id
 }
 
-locals {
-  third_octet_0   = random_integer.THIRD_OCTET_0.result
-  fourth_octet_0  = random_integer.FOURTH_OCTET_BASE_0.result * 4 + 1
-  third_octet_1   = random_integer.THIRD_OCTET_1.result
-  fourth_octet_1  = random_integer.FOURTH_OCTET_BASE_1.result * 4 + 1
-}
