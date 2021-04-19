378ddc595ba8645b02e8091692b63939b0dae3aa
J5 <john.heyer@hightail.com>
2021-04-19 14:41:52 -0700

    random ip test

--- terraform/gcp/ha_vpn/router_interfaces.tf
+++ terraform/gcp/ha_vpn/router_interfaces.tf
@@ -20,7 +20,7 @@ resource "google_compute_router_interface" "ROUTER_INTERFACE0" {
   name          = "${var.region}-${var.peer_gateway_name}-int0"
   region        = var.region
   #ip_range     = var.peer0_local_ip
-  ip_range      = "169.254.${third_octet_0}.${fourth_octet_0}"
+  ip_range      = "169.254.${local.third_octet_0}.${local.fourth_octet_0}"
   router        = var.cloud_router_name
   vpn_tunnel    = google_compute_vpn_tunnel.VPN_TUN0.id
 }
@@ -28,7 +28,7 @@ resource "google_compute_router_interface" "ROUTER_INTERFACE1" {
   name          = "${var.region}-${var.peer_gateway_name}-int1"
   region        = var.region
   #ip_range                        = var.peer1_local_ip
-  ip_range      = "169.254.${third_octet_1}.${fourth_octet_1}"
+  ip_range      = "169.254.${local.third_octet_1}.${local.fourth_octet_1}"
   router        = var.cloud_router_name
   vpn_tunnel    = google_compute_vpn_tunnel.VPN_TUN1.id
 }
