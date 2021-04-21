48b9c634b670c003c7ae154fb345c78ae1d0b932
J5 <john.heyer@hightail.com>
2021-04-20 20:55:06 -0700

    as

--- terraform/gcp/ha_vpn/outputs.tf
+++ terraform/gcp/ha_vpn/outputs.tf
@@ -4,8 +4,10 @@
 # Outputs for Secondary tunnel and BGP peer
 output vpn_tun0_psk { value = random_password.PEER0_PSK.*.result }
 output gcp_bgp0_neighbor_ip { value = google_compute_router_peer.ROUTER_VPN_BGP0.*.ip_address }
+output peer0_gcp_ip { value = google_compute_router_interface.ROUTER_INTERFACE0.*.ip_range }
 
 # Outputs for Secondary tunnel and BGP peer
 output vpn_tun1_psk { value = random_password.PEER1_PSK.*.result }
-output gcp_bgp1_neighbor_ip { value = google_compute_router_peer.ROUTER_VPN_BGP1.*.ip_address }
+output peer1_neighbor_ip { value = google_compute_router_peer.ROUTER_VPN_BGP1.*.ip_address }
+output peer1_gcp_ip { value = google_compute_router_interface.ROUTER_INTERFACE1.*.ip_range }
 
