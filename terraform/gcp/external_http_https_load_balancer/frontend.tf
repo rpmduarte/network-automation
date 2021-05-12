3c4adfcec63ac5e9a871ad8b6806a76338d6dbd7
J5 <john.heyer@hightail.com>
2021-05-12 02:03:12 -0700

    updates

--- terraform/gcp/external_http_https_load_balancer/frontend.tf
+++ terraform/gcp/external_http_https_load_balancer/frontend.tf
@@ -1,18 +1,37 @@
 
-# URL Map
-resource "google_compute_url_map" "URL_MAP" {
-  name               = "urlmap"
+# HTTP URL Map (just redirects to HTTPS)
+resource "google_compute_url_map" "URL_MAP_HTTP" {
+  name               = "urlmap-http"
   default_service    = google_compute_backend_bucket.BACKEND_BUCKET.id
+  depends_on         = [ google_compute_backend_bucket.BACKEND_BUCKET ]
+  host_rule {
+    hosts            = ["*"]
+    path_matcher     = "all-hosts"
+  }
+  path_matcher {
+    name             = "all-hosts"
+    default_url_redirect {
+      https_redirect = true
+      strip_query    = false
+    }
+  }
+  provider           = google-beta
+}
+resource "google_compute_url_map" "URL_MAP_HTTPS" {
+  count                 = length(var.regions)
+  name                  = "urlmap-https"
+  default_service       = google_compute_backend_service.BACKEND_SERVICE[count.index].id
 }
 
 # Target proxies
 resource "google_compute_target_http_proxy" "TPROXY_HTTP" {
   name                  = "target-proxy-http"
-  url_map               = google_compute_url_map.URL_MAP.id
+  url_map               = google_compute_url_map.URL_MAP_HTTP.id
 }
 resource "google_compute_target_https_proxy" "TPROXY_HTTPS" {
+  count                 = length(var.regions)
   name                  = "target-proxy-https"
-  url_map               = google_compute_url_map.URL_MAP.id
+  url_map               = google_compute_url_map.URL_MAP_HTTPS[count.index].id
   ssl_certificates      = [ 
     for cert in google_compute_ssl_certificate.SSL_CERT:
       cert.id
@@ -29,8 +48,9 @@ resource "google_compute_global_forwarding_rule" "FWD_RULE_HTTP" {
   target                = google_compute_target_http_proxy.TPROXY_HTTP.id
 }
 resource "google_compute_global_forwarding_rule" "FWD_RULE_HTTPS" {
+  count                 = length(var.regions)
   name                  = "frontend-https"
   ip_address            = google_compute_global_address.EXTERNAL_L7LB_IP_ADDRESS.address
   port_range            = "443"
-  target                = google_compute_target_https_proxy.TPROXY_HTTPS.id
+  target                = google_compute_target_https_proxy.TPROXY_HTTPS[count.index].id
 }
--- terraform/gcp/external_http_https_load_balancer/healthchecks.tf
+++ terraform/gcp/external_http_https_load_balancer/healthchecks.tf
@@ -1,7 +1,7 @@
 # Create Healthchecks
 resource "google_compute_health_check" "HTTP_CHECK_1" {
   name               = var.http_check1_name
-  https_health_check {
+  http_health_check {
     port             = var.http_check1_port
     request_path     = var.http_check1_path
   }
@@ -10,7 +10,7 @@ resource "google_compute_health_check" "HTTP_CHECK_1" {
 }
 resource "google_compute_health_check" "HTTP_CHECK_2" {
   name               = var.http_check2_name
-  https_health_check {
+  http_health_check {
     port             = var.http_check2_port
     request_path     = var.http_check2_path
   }
